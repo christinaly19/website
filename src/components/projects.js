6f116117b318f3de04b0ed6c8cf3a72e0ddfcd44
Christina Li <christinayxli@gmail.com>
2023-01-12 13:13:17 -0500

    small changes to colours, updated links

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,7 +17,7 @@ function Projects() {
                         <handwritten className = "tracking-wider subtitle_proj font-semibold text-xl"> Meraki </handwritten>
                         <p1 className = 'projectdes'> a web-app created with ReactJS, Tailwind, and Unsplash API. Designed to generate creative writing/journaling prompts and create a 
                         'moodboard' (collection of images) that reflect users' text using Unsplash's Keyword to Image API. </p1>
-                        <a href="https://github.com/christinaly19/TFT-Tierlist"><button type="button" className ="projectbutton border-2 rounded-lg hover:border-[#1D192E]" >View On Github</button> </a>
+                        <a href="https://github.com/christinaly19/meraki"><button type="button" className ="projectbutton border-2 rounded-lg hover:border-[#1D192E]" >View On Github</button> </a>
                 </div>
                 </div>
                 </div>
@@ -29,7 +29,7 @@ function Projects() {
                     <handwritten className = "tracking-wider subtitle_proj font-semibold text-xl">Sketchy Studios</handwritten>
                     <p1 className = 'projectdes'> a live, interactive web game with a custom canvas component, unique avatars, and real-time gameplay. built using react bootstrap (frontend) and socket.io, node.js and express (for backend). submitted to hack the north '21!</p1>
                     <div className = "wrap">
-                    <a href="https://github.com/christinaly19"><button type="button" class="projectbutton border-2 rounded-lg hover:border-[#1D192E] hover:bg-[#1D192E]">View On Github</button></a>
+                    <a href="https://github.com/christinaly19/sketchy-studio"><button type="button" class="projectbutton border-2 rounded-lg hover:border-[#1D192E] hover:bg-[#1D192E]">View On Github</button></a>
                     <a href = 'https://devpost.com/software/sketchy-studios'><button type="button" class="ml-2 projectbutton border-2 rounded-lg hover:border-[#1D192E]">View On Devpost</button></a>
                     </div>
                 </div>
@@ -42,7 +42,7 @@ function Projects() {
                     <handwritten className = "tracking-wider subtitle_proj font-semibold text-xl"> Tutor.ly</handwritten>
                     <p1 className = "projectdes"> a web-app built with React,html2canvas, and Material-UI. Helps tutors track student performance 
                     and input information easily & download into PDF. Also incorporates live Chat Bot to answer FAQs! </p1> 
-                    <button type="button" class="projectbutton border-2 rounded-lg hover:border-[#1D192E] hover-bg[#1D192E]">View On Github</button>
+                    <a href ='https://github.com/christinaly19/tutorly'><button type="button" class="projectbutton border-2 rounded-lg hover:border-[#1D192E] hover-bg[#1D192E]">View On Github</button></a>
                 </div>
                 </div>      
          
@@ -52,7 +52,7 @@ function Projects() {
                 <div className = "overlay4">
                 <handwritten className = "tracking-wider subtitle_proj font-semibold text-xl"> Job-Search</handwritten>
                     <p1 className = "projectdes"> a small, first project using web-scraping that searches for recent postings of writing job postings on timejobs. uses beautiful soup and python. </p1> 
-                    <button type="button" class="projectbutton border-2 rounded-lg hover:border-[#1D192E] hover-bg[#1D192E]">View On Github</button>
+                    <a href ='https://github.com/christinaly19/job-search'><button type="button" class="projectbutton border-2 rounded-lg hover:border-[#1D192E] hover-bg[#1D192E]">View On Github</button></a>
                 </div>
                 </div>
            
